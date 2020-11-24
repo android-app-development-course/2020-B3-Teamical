1358f9337604f354e320219539b8b1aee36d53e8
sjl623 <jinlongsong@m.scnu.edu.cn>
2020-11-24 17:11:02 +0800

    添加日程页面

--- Teamical/app/src/main/java/com/xuexiang/temical/fragment/NewEventFragment.java
+++ Teamical/app/src/main/java/com/xuexiang/temical/fragment/NewEventFragment.java
@@ -1,53 +1,62 @@
-/*
- * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.xuexiang.temical.fragment;
 
-import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 
-import com.xuexiang.temical.R;
-import com.xuexiang.temical.core.BaseFragment;
-import com.xuexiang.temical.utils.TokenUtils;
-import com.xuexiang.temical.utils.XToastUtils;
-import com.xuexiang.xaop.annotation.SingleClick;
 import com.xuexiang.xpage.annotation.Page;
 import com.xuexiang.xui.utils.ResUtils;
-import com.xuexiang.xui.utils.ThemeUtils;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
-import com.xuexiang.xui.widget.dialog.DialogLoader;
+import com.xuexiang.xui.widget.layout.ExpandableLayout;
+import com.xuexiang.xui.widget.picker.widget.OptionsPickerView;
+import com.xuexiang.xui.widget.picker.widget.TimePickerView;
+import com.xuexiang.xui.widget.picker.widget.builder.OptionsPickerBuilder;
+import com.xuexiang.xui.widget.picker.widget.builder.TimePickerBuilder;
+import com.xuexiang.xui.widget.picker.widget.configure.TimePickerType;
+import com.xuexiang.xui.widget.textview.badge.Badge;
+import com.xuexiang.xui.widget.textview.badge.BadgeView;
 import com.xuexiang.xui.widget.textview.supertextview.SuperTextView;
-import com.xuexiang.xutil.XUtil;
+import com.xuexiang.temical.R;
+import com.xuexiang.temical.core.BaseFragment;
+import com.xuexiang.temical.utils.XToastUtils;
+import com.xuexiang.xutil.data.DateUtils;
 
-/**
- * @author xuexiang
- * @since 2019-10-15 22:38
- */
-@Page(name = "新建日程")
-public class NewEventFragment extends BaseFragment implements SuperTextView.OnSuperTextViewClickListener {
+import java.util.Calendar;
 
+import butterknife.BindView;
+
+@Page(name = "NewEvent")
+public class NewEventFragment extends BaseFragment {
+    @BindView(R.id.stv_detail)
+    SuperTextView event_details;
+    @BindView(R.id.super_team_event)
+    SuperTextView team_event;
+    @BindView(R.id.expandable_layout)
+    ExpandableLayout mExpandableLayout;
+    @BindView(R.id.stv_repeat)
+    SuperTextView remind_view;
+    @BindView(R.id.super_remind)
+    SuperTextView alarm_remind;
+    @BindView(R.id.stv_start_time)
+    SuperTextView start_time;
+    @BindView(R.id.stv_end_time)
+    SuperTextView end_time;
+
+
+    private Badge mBadge;
+    private TimePickerView mStartTimePickerDialog;
+    private TimePickerView mendTimePickerDialog;
 
     @Override
     protected int getLayoutId() {
-        return R.layout.create_event;
+        return R.layout.fragment_create_event;
     }
 
     @Override
     protected void initViews() {
+        //设置空字符串用于占位
+//        event_details.setDefaultDrawable(null,null,null,0,0,0);
+//        event_details.setleft
 
     }
 
@@ -68,36 +77,64 @@ public class NewEventFragment extends BaseFragment implements SuperTextView.OnSu
         return titleBar;
     }
 
-
-    @SingleClick
     @Override
-    public void onClick(SuperTextView superTextView) {
-        switch (superTextView.getId()) {
-            case R.id.menu_common:
-            case R.id.menu_privacy:
-            case R.id.menu_push:
-            case R.id.menu_helper:
-                XToastUtils.toast(superTextView.getLeftString());
-                break;
-            case R.id.menu_change_account:
-                XToastUtils.toast(superTextView.getCenterString());
-                break;
-            case R.id.menu_logout:
-                DialogLoader.getInstance().showConfirmDialog(
-                        getContext(),
-                        getString(R.string.lab_logout_confirm),
-                        getString(R.string.lab_yes),
-                        (dialog, which) -> {
-                            dialog.dismiss();
-                            XUtil.getActivityLifecycleHelper().exit();
-                            TokenUtils.handleLogoutSuccess();
-                        },
-                        getString(R.string.lab_no),
-                        (dialog, which) -> dialog.dismiss()
-                );
-                break;
-            default:
-                break;
-        }
+    protected void initListeners() {
+        String[] mRepeatOption = ResUtils.getStringArray(R.array.repeat);
+        String[] mRemindOption = ResUtils.getStringArray(R.array.remind);
+        team_event.setOnSuperTextViewClickListener(superTextView -> superTextView.setSwitchIsChecked(!superTextView.getSwitchIsChecked(), false)).setSwitchCheckedChangeListener((buttonView, isChecked) -> mExpandableLayout.toggle());
+        remind_view.setOnSuperTextViewClickListener(superTextView -> {
+            OptionsPickerView pvOptions = new OptionsPickerBuilder(getContext(), (v, options1, options2, options3) -> {
+
+                return false;
+            })
+                    .setTitleText("提醒周期")
+                    .setSelectOptions(0)
+                    .build();
+            pvOptions.setPicker(mRepeatOption);
+            pvOptions.show();
+        });
+        alarm_remind.setOnSuperTextViewClickListener(superTextView -> {
+            OptionsPickerView pvOptions = new OptionsPickerBuilder(getContext(), (v, options1, options2, options3) -> {
+
+                return false;
+            })
+                    .setTitleText("提醒周期")
+                    .setSelectOptions(0)
+                    .build();
+            pvOptions.setPicker(mRemindOption);
+            pvOptions.show();
+        });
+        start_time.setOnSuperTextViewClickListener(superTextView -> {
+            if (mStartTimePickerDialog == null) {
+                Calendar calendar = Calendar.getInstance();
+
+                mStartTimePickerDialog = new TimePickerBuilder(getContext(), (date, v) -> XToastUtils.toast(DateUtils.date2String(date, DateUtils.yyyyMMddHHmmss.get())))
+                        .setTimeSelectChangeListener(date -> Log.i("pvTime", "onTimeSelectChanged"))
+                        .setType(TimePickerType.ALL)
+                        .setTitleText("时间选择")
+                        .isDialog(true)
+                        .setOutSideCancelable(false)
+                        .setDate(calendar)
+                        .build();
+            }
+            mStartTimePickerDialog.show();
+        });
+
+        end_time.setOnSuperTextViewClickListener(superTextView -> {
+            if (mStartTimePickerDialog == null) {
+                Calendar calendar = Calendar.getInstance();
+
+                mStartTimePickerDialog = new TimePickerBuilder(getContext(), (date, v) -> XToastUtils.toast(DateUtils.date2String(date, DateUtils.yyyyMMddHHmmss.get())))
+                        .setTimeSelectChangeListener(date -> Log.i("pvTime", "onTimeSelectChanged"))
+                        .setType(TimePickerType.ALL)
+                        .setTitleText("时间选择")
+                        .isDialog(true)
+                        .setOutSideCancelable(false)
+                        .setDate(calendar)
+                        .build();
+            }
+            mStartTimePickerDialog.show();
+        });
     }
+
 }
