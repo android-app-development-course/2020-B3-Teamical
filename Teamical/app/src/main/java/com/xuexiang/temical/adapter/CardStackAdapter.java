7aa2babb56bc34bf760d53044dd8a57fa3b185b9
sjl623 <jinlongsong@m.scnu.edu.cn>
2020-11-25 12:00:06 +0800

    团队界面控件引入

---
+++ Teamical/app/src/main/java/com/xuexiang/temical/adapter/CardStackAdapter.java
@@ -0,0 +1,79 @@
+package com.xuexiang.temical.adapter;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.xuexiang.temical.R;
+import com.xuexiang.temical.adapter.entity.Spot;
+
+import java.util.List;
+
+public class CardStackAdapter extends RecyclerView.Adapter<CardStackAdapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+    private List<Spot> spots;
+
+    public CardStackAdapter(Context context, List<Spot> spots) {
+        this.inflater = LayoutInflater.from(context);
+        this.spots = spots;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.item_spot, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Spot spot = spots.get(position);
+        holder.name.setText(spot.name);
+        holder.city.setText(spot.city);
+        Glide.with(holder.image)
+                .load(spot.url)
+                .into(holder.image);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), spot.name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return spots.size();
+    }
+
+    public List<Spot> getSpots() {
+        return spots;
+    }
+
+    public void setSpots(List<Spot> spots) {
+        this.spots = spots;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView city;
+        ImageView image;
+        ViewHolder(View view) {
+            super(view);
+            this.name = view.findViewById(R.id.item_name);
+            this.city = view.findViewById(R.id.item_city);
+            this.image = view.findViewById(R.id.item_image);
+        }
+    }
+
+}
--- Teamical/app/src/main/java/com/xuexiang/temical/fragment/TeamFragment.java
+++ Teamical/app/src/main/java/com/xuexiang/temical/fragment/TeamFragment.java
@@ -17,18 +17,54 @@
 
 package com.xuexiang.temical.fragment;
 
+import android.view.View;
+
 import com.xuexiang.temical.R;
+import com.xuexiang.temical.adapter.CardStackAdapter;
+import com.xuexiang.temical.adapter.entity.Spot;
 import com.xuexiang.temical.core.BaseFragment;
 import com.xuexiang.xpage.annotation.Page;
 import com.xuexiang.xpage.enums.CoreAnim;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * @author xuexiang
  * @since 2019-10-30 00:19
  */
 @Page(anim = CoreAnim.none)
-public class TeamFragment extends BaseFragment {
+public class TeamFragment extends BaseFragment implements CardStackListener {
+    private List<Spot> createSpots() {
+        List<Spot> spots = new ArrayList<>();
+        spots.add(new Spot("Yasaka Shrine", "Kyoto", "https://source.unsplash.com/Xq1ntWruZQI/600x800"));
+        spots.add(new Spot("Fushimi Inari Shrine", "Kyoto", "https://source.unsplash.com/NYyCqdBOKwc/600x800"));
+        spots.add(new Spot("Bamboo Forest", "Kyoto", "https://source.unsplash.com/buF62ewDLcQ/600x800"));
+        spots.add(new Spot("Brooklyn Bridge", "New York", "https://source.unsplash.com/THozNzxEP3g/600x800"));
+        spots.add(new Spot("Empire State Building", "New York", "https://source.unsplash.com/USrZRcRS2Lw/600x800"));
+        spots.add(new Spot("The statue of Liberty", "New York", "https://source.unsplash.com/PeFk7fzxTdk/600x800"));
+        spots.add(new Spot("Louvre Museum", "Paris", "https://source.unsplash.com/LrMWHKqilUw/600x800"));
+        spots.add(new Spot("Eiffel Tower", "Paris", "https://source.unsplash.com/HN-5Z6AmxrM/600x800"));
+        spots.add(new Spot("Big Ben", "London", "https://source.unsplash.com/CdVAUADdqEc/600x800"));
+        spots.add(new Spot("Great Wall of China", "China", "https://source.unsplash.com/AWh9C-QjhE4/600x800"));
+        return spots;
+    }
+
+
+    private CardStackLayoutManager manager;
+    private CardStackAdapter adapter;
+    @BindView(R.id.card_stack_view)
+    CardStackView cardStackView;
+
 
     /**
      * @return 返回为 null意为不需要导航栏
@@ -45,7 +81,7 @@ public class TeamFragment extends BaseFragment {
      */
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_trending;
+        return R.layout.fragment_team;
     }
 
     /**
@@ -53,6 +89,49 @@ public class TeamFragment extends BaseFragment {
      */
     @Override
     protected void initViews() {
+        manager = new CardStackLayoutManager(this.getActivity(), this);
+        manager.setStackFrom(StackFrom.None);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.VERTICAL);
+        manager.setCanScrollHorizontal(false);
+        manager.setCanScrollVertical(true);
+        adapter = new CardStackAdapter(this.getActivity(), createSpots());
+        cardStackView = findViewById(R.id.card_stack_view);
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCardDragging(Direction direction, float ratio) {
+
+    }
+
+    @Override
+    public void onCardSwiped(Direction direction) {
+
+    }
+
+    @Override
+    public void onCardRewound() {
+
+    }
+
+    @Override
+    public void onCardCanceled() {
+
+    }
+
+    @Override
+    public void onCardAppeared(View view, int position) {
+
+    }
+
+    @Override
+    public void onCardDisappeared(View view, int position) {
 
     }
 }
